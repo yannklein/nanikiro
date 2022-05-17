928a88b072671e02c17171dc716b1c09bb965c0e
Yann Klein <yann.klein@me.com>
2022-05-17 13:48:01 +0900

    closets crrontroller action

--- app/controllers/closets_controller.rb
+++ app/controllers/closets_controller.rb
@@ -1,16 +1,42 @@
 class ClosetsController < ApplicationController
+  before_action :find_closet, only: [:show, :update]
   def index
+    @closets = Closet.all
   end
 
   def show
   end
 
   def create
+    @closet = Closet.new(closet_params)
+    @closet.user = current_user
+    if @closet.save
+      redirect_to closet_path(@closet)
+    else
+      render :index
+    end
   end
 
   def edit
+    if @closet.update(closet_params)
+      redirect_to closet_path(@closet)
+    else
+      render :index
+    end
   end
 
   def destroy
+    @closet.destroy
+    redirect_to closets_path
+  end
+
+  private 
+
+  def find_closet
+    Closet.find(params[:id])
+  end
+
+  def closet_params
+    params.require(:closet).permit(:name)
   end
 end
