73b49b1d8907d2543bcf014b2d0a110041b68851
Yann Klein <yann.klein@me.com>
2022-05-17 13:52:24 +0900

    wear controller actions

--- app/controllers/wears_controller.rb
+++ app/controllers/wears_controller.rb
@@ -1,10 +1,37 @@
 class WearsController < ApplicationController
+  before_action :find_wear, only: [:update, :destroy]
+
   def create
+    @wear = Wear.new(wear_params)
+    @closet = Closet.find(params[:closet_id])
+    @wear.closet = @closet
+    if @wear.save
+      redirect_to wear_path(@wear)
+    else
+      render 'closet/index'
+    end
   end
 
   def destroy
+    @wear.destroy
+    redirect_to closets_path
   end
 
   def update
+    if @wear.update(wear_params)
+      redirect_to wear_path(@wear)
+    else
+      render 'closet/index'
+    end
+  end
+
+  private 
+
+  def find_wear
+    Wear.find(params[:id])
+  end
+
+  def wear_params
+    params.require(:wear).permit(:season, :wear_type, :closet_id, :photo, :color)
   end
 end
