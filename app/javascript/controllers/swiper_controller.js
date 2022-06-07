a59361a7060dba8937e1d5256c99431b3c5d814f
Yann Klein <yann.klein@me.com>
2022-06-07 13:33:24 +0900

    added clothes list

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,14 +1,28 @@
 import { Controller } from '@hotwired/stimulus'
-import Swiper, { EffectCards } from 'swiper';
+import Swiper, { EffectCards, Navigation, Thumbs, FreeMode } from 'swiper';
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
 
 export default class extends Controller {
+  static targets = [ 'preview', 'slider' ]
+
   connect() {
-    new Swiper(this.element, {
-      modules: [EffectCards],
+    var preview = new Swiper(this.previewTarget, {
+      modules: [FreeMode],
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true
+    })
+
+    new Swiper(this.sliderTarget, {
+      modules: [EffectCards, Thumbs],
       effect: "cards",
       grabCursor: true,
-    });
+      spaceBetween: 10,
+      thumbs: {
+        swiper: preview,
+      },
+    })
   }
 }
