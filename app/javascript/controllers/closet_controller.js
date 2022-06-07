b39dfc2503eff0c45c79b574d15f0e91132d76df
Yann Klein <yann.klein@me.com>
2022-06-07 14:27:15 +0900

    built scrolling logic

---
+++ app/javascript/controllers/closet_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  static targets = [ 'link' ]
+
+  connect() {
+    console.log('Hello from mannequin-controller.js')
+    // console.log(this.testTarget)
+  }
+  scroll(e){
+    e.preventDefault()
+    console.log(e.currentTarget);
+    document.getElementById(e.currentTarget.href.split("#")[1]).scrollIntoView()
+  }
+}
