da5e4dbe6f0ee0e450c0d37e02f62b7172f96634
Yann Klein <yann.klein@me.com>
2022-10-20 18:50:37 +0900

    reflect todays item shwoing

--- app/models/closet.rb
+++ app/models/closet.rb
@@ -2,4 +2,8 @@ class Closet < ApplicationRecord
   belongs_to :user
   has_many :wears
   validates :name, presence: true, uniqueness: true
+
+  def todays(category)
+    TodaysItem.where(closet: self, weared_on: Date.today).find { |item| item.wear.category == category}
+  end
 end
